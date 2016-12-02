82a5d58a09a22d01e09f819da83ac7519b5b1da8
Melissa Beck <beck.melissa@gmx.ch>
2016-12-02 15:58:08 +0100

    created view package and the classes inside

---
+++ VierGewinnt/src/View/GameView.java
@@ -0,0 +1,44 @@
+/*
+* Class created by Melissa Beck. Hochschule Luzern - Informatik. Copyright 2016
+*
+* Change log:
+*
+* Who               When        Signature       What
+* ------------------------------------------------------------------------------------------------------------------
+* M. Beck       01.12.2016  MB20161201_01   Created the class, implemented its methods and added JavaDoc.      
+*/
+
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Shows the GameView, depicts the current state of the game and shows the playername and its score.
+ * @author Melissa Beck
+ */
+public class GameView {
+    
+    /**
+    * Creates an instance of the GameView.
+    * param color The color of the token.
+    * return The {@code Color} of the GameView.
+    */
+    public GameView() {
+    
+    }
+    
+    /**
+    * Shows if the player has won or 
+    * param color The color of the token.
+    * return The {@code Color} of the GameView.
+    */
+    public void showResult() {
+    
+    }
+    
+    public void updateBoard() {
+        
+    }
+    
+}
---
+++ VierGewinnt/src/View/MainMenu.java
@@ -0,0 +1,19 @@
+/*
+* Class created by Melissa Beck. Hochschule Luzern - Informatik. Copyright 2016
+*
+* Change log:
+*
+* Who               When        Signature       What
+* ------------------------------------------------------------------------------------------------------------------
+* M. Beck       01.12.2016  MB20161201_01   Created the class      
+*/
+
+package View;
+
+/**
+ *
+ * @author Melissa
+ */
+public class MainMenu {
+    
+}
