4024054101456343b00b5081880b3ec9b049fb9a
Melissa Beck <beck.melissa@gmx.ch>
2016-12-11 20:42:43 +0100

    implemented the paint method and added the new Class CustomLabel

--- VierGewinnt/src/View/GameView.java
+++ VierGewinnt/src/View/GameView.java
@@ -8,13 +8,14 @@
 * M. Beck       01.12.2016  MB20161201_01   Created the class, implemented method stubs and added JavaDoc.
 * M. Beck       06.12.2016  MB20161206_01   Implemented the GUI without functionality
 * M. Beck       08.12.2016  MB20161208_01   Implemented the GUI without functionality
+* M. Beck       11.12.2016  MB20161211_01   Implemented the setting of a token
 */
 
 package View;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
+import Model.GameBoard;
 
 /**
  * Shows the GameView, depicts the current state of the game and shows the playername and its score.
@@ -26,21 +27,25 @@ public class GameView {
     private String opponent;
     private int currentPlayerScore;
     private int opponentScore;
-    // the xsize and the ysize are not very dynamic
     private int xsize = 7;
-    private int ysize = 6;
+    private int ysize = 8;
+    private JButton[] buttons;
+    private GameBoard gameBoard;
+    private CustomLabel[][] fields;
     
     /**
     * Creates an instance of the GameView.
     */
-    public GameView(final int sxSize, final int sySize) {
-        xsize = sxSize;
-        ysize = sySize;
-        JFrame frame = new JFrame("Vier gewinnt");        
+    public GameView() {
+    // public GameView(final GameBoard gameBoard) {
+        // this.gameBoard = gameBoard;
+        // xsize = gameBoard.getWidth();
+        // ysize = gameBoard.getHeight();
+        JFrame frame = new JFrame("Vier gewinnt");
         Container contentPane = frame.getContentPane();
         JPanel gameGrid = new JPanel();
-        JLabel[][] fields = new JLabel[xsize][ysize];
-        JButton[] buttons = new JButton[xsize];
+        fields = new CustomLabel[xsize][ysize];
+        buttons = new JButton[xsize];
 
         // adding the buttons
         for (int i = 0; i < xsize; i++) {
@@ -51,26 +56,45 @@ public class GameView {
         // adding the fields
         for (int column = 0; column < ysize; column++) {
             for (int row = 0; row < xsize; row++) {
-                fields[row][column] = new JLabel();
+                fields[row][column] = new CustomLabel(Color.RED);
                 fields[row][column].setHorizontalAlignment(SwingConstants.CENTER);
-                fields[row][column].setBorder(new LineBorder(Color.blue, 3));
+                fields[row][column].setOpaque(true);
+                fields[row][column].setBackground(Color.blue);
                 gameGrid.add(fields[row][column]);
             }
         }
-
+        
         JLabel memberName = new JLabel("Your name is: " + currentPlayer);
         JLabel scoreTable = new JLabel("Score: " + currentPlayer + " " + currentPlayerScore + " – " + opponent + " " + opponentScore);
         
         contentPane.add(memberName, BorderLayout.PAGE_START);
         contentPane.add(gameGrid, BorderLayout.CENTER);
         contentPane.add(scoreTable, BorderLayout.PAGE_END);
-        gameGrid.setLayout(new GridLayout(xsize, ysize + 1));
+        gameGrid.setLayout(new GridLayout(ysize + 1, xsize + 1));
         
-        frame.pack();
+        frame.setSize(500, 600);
         frame.setVisible(true);
     }
     
+     /**
+     * Updates the Token in the Gamboard.
+     * @param row The specific row to update
+     * @param column The specific column to update
+     * @param color The specific color from the player to update
+     */
+    public void updateGameboard(int row, int column, Color color) {
+        fields[row][column].updateColor(color);
+    }
+    
+    /**
+     * Get the buttons of the GameBoard.
+     * @return an array with the JButtons in it.
+     */
+    public JButton[] getButtons() {
+        return buttons;
+    }
+    
 //    public static void main(String [ ] args) {
-//        GameView view = new GameView();
+//        GameView gameView = new GameView();
 //    }
 }
